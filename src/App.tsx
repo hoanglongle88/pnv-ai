dfe7d2765f5b6842cbfa0ec2443c43eb2548bd24
hoanglongle88 <long.le@stunited.vn>
2025-12-18 17:25:08 +0700

    feat: [PNV-1] Commit  test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='bg-black min-h-screen flex items-center justify-center'>
-      <h1 className='text-center text-[#21baea] text-[5rem]'>ANV AI Platform</h1>
+      <h1 className='text-center text-[#21baea] text-[5rem]'>ANV AI Platform Commit Test</h1>
     </div>
   );
 }
