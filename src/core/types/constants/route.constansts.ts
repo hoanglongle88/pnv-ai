b20a299c5ec32d3716ed39bdc7a761a27b0996b2
hoanglongle88 <long.le@stunited.vn>
2025-12-19 11:47:13 +0700

    feat: [PNV-1] Add route constant and update commit-msg

---
+++ src/core/types/constants/route.constansts.ts
@@ -0,0 +1,9 @@
+// src/core/routes/route.constants.ts
+export const ROUTES = {
+  HOME: '/home',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  USER_DASHBOARD: '/dashboard',
+  ADMIN_DASHBOARD: '/admin/dashboard',
+  ADMIN_PANEL: '/admin/panel',
+};
